8e0b3ddade5013fbcaf9d075228773375d2bc74d
hallow0312 <leejaehyun0312@naver.com>
2024-05-10 21:44:50 +0900

    2024.05.10.스크립트_생성_및_사용

---
+++ 이재현 Unity/Program/Assets/Class2th(Export_Package)/Scripts/Data.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+  public int data = 10;
+    
+  public void Start()
+    {
+        Debug.Log(data);
+    }  
+}
