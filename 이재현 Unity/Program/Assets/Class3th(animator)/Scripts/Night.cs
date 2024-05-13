eebc3b682d7f8cb800b17c3965df2a56b07409af
hallow0312 <leejaehyun0312@naver.com>
2024-05-13 21:44:48 +0900

    2024.05.13.애니메이터 컨트롤러

---
+++ 이재현 Unity/Program/Assets/Class3th(animator)/Scripts/Night.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Night : MonoBehaviour
+{
+    // 값 타입 // 참조 타입  
+    //값 타입 : char , byte ,bool ,short ,int ,long , double ,decimal, struct
+    // 참조 타입 : class , array , collection , delegate, interface, abstractclass ,event;
+    // int [] array = new int [5];
+    // class Doozy = new Doozy();
+
+    // 최상위 타입 : object : 값 타입 참조 타입 다담을 수 있는 타입  - 참조타입으로 되어있다 .
+    
+
+}
