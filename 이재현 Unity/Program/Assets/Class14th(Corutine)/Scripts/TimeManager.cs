a55923250ca5046a44e45523efa44b01daa6ae72
hallow0312 <leejaehyun0312@naver.com>
2024-05-29 20:36:50 +0900

    2024

---
+++ 이재현 Unity/Program/Assets/Class14th(Corutine)/Scripts/TimeManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float time;
+  
+    void Start()
+    {
+        
+    }
+
+    
+}
