a437246b0402b6880ddcfa1cb6247e8f244d4e1b
hallow0312 <leejaehyun0312@naver.com>
2024-05-30 21:43:10 +0900

    2024.05.30.애니메이션

---
+++ 이재현 Unity/Program/Assets/Class15th(AnimationEvent)/Script/Bot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class Bot : MonoBehaviour  
+{ 
+      public void Attack()
+    {
+        Debug.Log("Attack");
+    }
+
+
+  
+}
+
